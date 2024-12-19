de309aca2e4f4921084abf9baba2f437fcb8aec4
sungyeon <wangkobong@gmail.com>
2024-12-19 15:56:21 +0900

    feat - 홈화면 데이터 서버에서 fetch

--- Projects/Feature/Sources/Dependencies/HomeClient.swift
+++ Projects/Feature/Sources/Dependencies/HomeClient.swift
@@ -8,25 +8,22 @@
 import Foundation
 import ComposableArchitecture
 import Models
+import FirebaseModule
 
 public struct HomeClient {
     var fetch: () async throws -> HomeData
     
     static let live = Self {
-        guard let url = Bundle.main.url(forResource: "RecommendRestaurantDummy", withExtension: "json"),
-              let data = try? Data(contentsOf: url) else {
-            throw HTTPError.invalidURL
-        }
+        let data = try await FirebaseClient.live.getApprovedRestaurants()
         
-//        print(Bundle.main.bundlePath)  // 번들 경로 출력
-//        if let url = Bundle.main.url(forResource: "RecommendRestaurantDummy", withExtension: "json") {
-//            print("Found JSON at: \(url)")
-//        } else {
-//            print("JSON file not found")
-//        }
+        // 콜키지 무료인 식당들 필터링
+        let corkageFreeRestaurants = data.restaurants.filter { $0.isCorkageFree }
         
-        let decoder = JSONDecoder()
-        return try decoder.decode(HomeData.self, from: data)
+        // 추천 식당은 전체 식당 중에서 랜덤으로 선택하거나,
+        // 특정 조건으로 필터링할 수 있습니다
+        let recommendedRestaurants = Array(data.restaurants.shuffled().prefix(5))  // 예시로 랜덤 5개 선택
+        return HomeData(recommenderRestaurants: recommendedRestaurants,
+                        corkageFreerestaurants: corkageFreeRestaurants)
     }
 }
 
--- Projects/Models/Sources/HomeData.swift
+++ Projects/Models/Sources/HomeData.swift
@@ -10,4 +10,15 @@ import Foundation
 public struct HomeData: Codable, Equatable {
     public let recommenderRestaurants: [RestaurantCard]
     public let corkageFreerestaurants: [RestaurantCard]
+    
+    public init(recommenderRestaurants: [RestaurantCard], corkageFreerestaurants: [RestaurantCard]) {
+        self.recommenderRestaurants = recommenderRestaurants
+        self.corkageFreerestaurants = corkageFreerestaurants
+    }
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.recommenderRestaurants = try container.decode([RestaurantCard].self, forKey: .recommenderRestaurants)
+        self.corkageFreerestaurants = try container.decode([RestaurantCard].self, forKey: .corkageFreerestaurants)
+    }
 }
